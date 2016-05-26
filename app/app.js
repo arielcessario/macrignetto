d7815697c32b599b512c0a101e6d4a6c202a3406
arielcessario <arielcessario@gmail.com>
2016-05-26 18:42:41 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on views, and components
     angular.module('macrignetto', ['oc.lazyLoad',
         'ngRoute',
+        'ngAnimate',
         'firebase',
         'textAngular',
         'acUtils',
